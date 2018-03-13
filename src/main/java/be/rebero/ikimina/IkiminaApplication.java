d078076c17e4109bbdfe6db3eb10cd53acfe8fd7
rmedard <00Derfine@5>
2018-03-13 01:06:58 +0100

    W10

---
+++ src/main/java/be/rebero/ikimina/IkiminaApplication.java
@@ -0,0 +1,12 @@
+package be.rebero.ikimina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkiminaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkiminaApplication.class, args);
+	}
+}
